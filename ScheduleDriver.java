cfb0f8c4b8a0fc20c3ce25ff3147c5765e629c79
jprend357 <jprend357@gmail.com>
2016-11-12 22:06:26 -0500

    Update ScheduleDriver.java

--- ScheduleDriver.java
+++ ScheduleDriver.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class ScheduleDriver {
 
 	public static void main(String[] args)throws IOException{
-		System.out.println("test");
+		System.out.println("test2");
 		
 		}
 	}
